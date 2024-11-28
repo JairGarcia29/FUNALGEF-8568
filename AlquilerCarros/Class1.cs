738d3f6f24c9903008c106f801922bbe23f3d529
JairGarcia29 <jamesjair29@gmail.com>
2024-11-28 09:20:26 -0500

    Commit FINAL

--- AlquilerCarros/Class1.cs
+++ AlquilerCarros/Class1.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -39,17 +42,69 @@ namespace AlquilerCarros
     }
     public class Clienteutil
     {
-        public int Numero { get; set; }
-        public Carro CarroAlq { get; set; }
-        public Cliente Contratante { get; set; }
-        public double Precio { get; set; }
-        public override string? ToString()
+        private static Cliente[] clientes = new Cliente[10];
+
+        private static int contador = 0;
+        public static void Crear()
         {
-            return $"Numero: {Numero}\nCarro: {CarroAlq}\nCliente: {Contratante}\nPrecio :{Precio}";
+            if (contador >= clientes.Length)
+            {
+                Console.WriteLine("No se pueden agregar más clientes, el arreglo está lleno.");
+                return;
+            }
+            Cliente c = new Cliente();
+            Console.WriteLine("Ingrese DNI del cliente: ");
+            c.Dni = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese nombre del cliente: ");
+            c.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese pago máximo: ");
+            c.PagoMaximo = double.Parse(Console.ReadLine());
+            clientes[contador] = c;
+            contador++;
+            Console.WriteLine("Cliente creado exitosamente.");
         }
-    }
+        public static void Listar()
+        {
+            if (contador == 0)
+            {
+                Console.WriteLine("No hay clientes registrados.");
+
+                return;
+            }
+            for (int i = 0; i < contador; i++)
+            {
+                Console.WriteLine(clientes[i].ToString());
+            }
+        }
+        public static void Eliminar()
+        {
+            if (contador == 0)
+            {
+                Console.WriteLine("No hay clientes para eliminar.");
+                return;
+            }
+            Console.WriteLine("Ingrese el DNI del cliente a eliminar:");
+            int dni = int.Parse(Console.ReadLine());
+            bool encontrado = false;
+            for (int i = 0; i < contador; i++)
+            {
+                if (clientes[i].Dni == dni)
+                {
+                    for (int j = i; j < contador - 1; j++)
+                    {
+                        clientes[j] = clientes[j + 1];
+                    }
+                    clientes[contador - 1] = null;
+                    contador--;
+                    Console.WriteLine("Cliente eliminado exitosamente.");
+                    encontrado = true;
+                    break;
+                }
+            }
+            if (!encontrado)
+            {
+                Console.WriteLine("Cliente no encontrado.");
+            }
+        }
+    }         
 }
-//holaa
-//555
-//554
-//1234
